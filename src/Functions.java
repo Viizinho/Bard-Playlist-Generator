f71e970df05300c44a4d4caa889d7ff5af1329dc
Viizinho <joaovitorbeltrao@gmail.com>
2024-08-25 19:45:55 -0300

    feat: add functions to add and remove music

--- src/Functions.java
+++ src/Functions.java
@@ -1,13 +1,109 @@
+import java.io.*;
+import java.util.*;
+
 public class Functions {
-    public static void criarPlaylist(String name) {
-        System.out.println("Creating your playlist");
+    private static final String PLAYLISTS_DIR = "Playlists/";
+
+    static {
+        File dir = new File(PLAYLISTS_DIR);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+    }
+
+    public static void createPlaylist(String name) {
+        File playlistFile = new File(PLAYLISTS_DIR + name + ".txt");
+        try {
+            if (playlistFile.createNewFile()) {
+                System.out.println("Playlist created: " + name);
+            } else {
+                System.out.println("Playlist already exists: " + name);
+            }
+        } catch (IOException e) {
+            System.err.println("Error creating playlist file: " + e.getMessage());
+        }
     }
 
     public static void deletePlaylist(String name) {
-        System.out.println("Deleting your playlist");
+        File playlistFile = new File(PLAYLISTS_DIR + name + ".txt");
+        if (playlistFile.delete()) {
+            System.out.println("Playlist deleted: " + name);
+        } else {
+            System.out.println("Playlist not found or could not be deleted: " + name);
+        }
     }
 
     public static void displayPlaylist(String name) {
-        System.out.println("Displaying your playlist");
+        File playlistFile = new File(PLAYLISTS_DIR + name + ".txt");
+        if (!playlistFile.exists()) {
+            System.out.println("Playlist not found: " + name);
+            return;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(playlistFile))) {
+            String line;
+            System.out.println("Playlist: " + name);
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading from playlist file: " + e.getMessage());
+        }
+    }
+
+    public static void createSong(String playlist, String song) {
+        File playlistFile = new File(PLAYLISTS_DIR + playlist + ".txt");
+        if (!playlistFile.exists()) {
+            System.out.println("Playlist not found: " + playlist);
+            return;
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(playlistFile, true))) {
+            writer.write(song);
+            writer.newLine();
+            System.out.println("Song added to playlist: " + playlist);
+        } catch (IOException e) {
+            System.err.println("Error writing to playlist file: " + e.getMessage());
+        }
+    }
+
+    public static void deleteSong(String playlist, String song) {
+        File playlistFile = new File(PLAYLISTS_DIR + playlist + ".txt");
+        if (!playlistFile.exists()) {
+            System.out.println("Playlist not found: " + playlist);
+            return;
+        }
+
+        File tempFile = new File(PLAYLISTS_DIR + "temp_" + playlist + ".txt");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(playlistFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
+
+            String line;
+            boolean found = false;
+            while ((line = reader.readLine()) != null) {
+                if (!line.trim().equals(song)) {
+                    writer.write(line);
+                    writer.newLine();
+                } else {
+                    found = true;
+                }
+            }
+
+            if (found) {
+                System.out.println("Song removed from playlist: " + playlist);
+            } else {
+                System.out.println("Song not found in playlist: " + playlist);
+            }
+        } catch (IOException e) {
+            System.err.println("Error processing playlist file: " + e.getMessage());
+        }
+
+        if (!playlistFile.delete()) {
+            System.err.println("Could not delete original playlist file");
+        }
+        if (!tempFile.renameTo(playlistFile)) {
+            System.err.println("Could not rename temporary file");
+        }
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -10,44 +10,53 @@ public class Menu {
             System.out.println("1. Create a new playlist");
             System.out.println("2. Delete a playlist");
             System.out.println("3. Display playlist");
-            System.out.println("4. Exit");
+            System.out.println("4. Create music to a playlist");
+            System.out.println("5. Delete music of a playlist");
+            System.out.println("0. Exit");
 
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
                     System.out.println("Please enter the name of the playlist:");
-                    String name1 = scanner.next();
-                    criarPlaylist(name1);
+                    String namePlaylist1 = scanner.next();
+                    Functions.createPlaylist(namePlaylist1);
                     break;
+
                 case 2:
                     System.out.println("Please enter the name of the playlist:");
-                    String name2 = scanner.next();
-                    deletePlaylist(name2);
+                    String namePlaylist2 = scanner.next();
+                    Functions.deletePlaylist(namePlaylist2);
                     break;
+
                 case 3:
                     System.out.println("Please enter the name of the playlist:");
-                    String name3 = scanner.next();
-                    displayPlaylist(name3);
+                    String namePlaylist3 = scanner.next();
+                    Functions.displayPlaylist(namePlaylist3);
                     break;
+
                 case 4:
+                    System.out.println("Please enter the name of the Playlist");
+                    String namePlaylist4 = scanner.next();
+                    System.out.println("Please enter the name of the Song:");
+                    String nameSong4 = scanner.next();
+                    Functions.createSong(namePlaylist4, nameSong4);
+                    break;
+
+                case 5:
+                    System.out.println("Please enter the name of the Playlist");
+                    String namePlaylist5 = scanner.next();
+                    System.out.println("Please enter the name of the Song:");
+                    String nameSong5 = scanner.next();
+                    Functions.deleteSong(namePlaylist5, nameSong5);
+                    break;
+                case 0:
                     System.out.println("Thank you for using Bard Playlist Generator!");
                     break;
 
                 default:
                     System.out.println("Invalid option");
             }
-        } while (opcao != 4);
-    }
-    private void criarPlaylist(String name) {
-        Functions.criarPlaylist(name);
-    }
-
-    private void deletePlaylist(String name) {
-        Functions.deletePlaylist(name);
-    }
-
-    private void displayPlaylist(String name) {
-        Functions.displayPlaylist(name);
+        } while (opcao != 0);
     }
 }
