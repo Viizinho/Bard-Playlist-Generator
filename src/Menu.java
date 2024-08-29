229848bd57f493795d36f3536725045b28cb7423
Viizinho <joaovitorbeltrao@gmail.com>
2024-08-28 23:47:52 -0300

    fix: changed all the next to nextLine

--- src/Menu.java
+++ src/Menu.java
@@ -15,35 +15,36 @@ public class Menu {
             System.out.println("0. Exit");
 
             opcao = scanner.nextInt();
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
                     System.out.println("Please enter the name of the playlist:");
-                    String namePlaylist1 = scanner.next();
+                    String namePlaylist1 = scanner.nextLine();
                     Functions.createPlaylist(namePlaylist1);
                     break;
 
                 case 2:
                     System.out.println("Please enter the name of the playlist:");
-                    String namePlaylist2 = scanner.next();
+                    String namePlaylist2 = scanner.nextLine();
                     Functions.deletePlaylist(namePlaylist2);
                     break;
 
                 case 3:
                     System.out.println("Please enter the name of the playlist:");
-                    String namePlaylist3 = scanner.next();
+                    String namePlaylist3 = scanner.nextLine();
                     Functions.displayPlaylist(namePlaylist3);
                     break;
 
                 case 4:
                     System.out.println("Please enter the name of the Playlist:");
-                    String namePlaylist4 = scanner.next();
+                    String namePlaylist4 = scanner.nextLine();
                     System.out.println("Please enter the title of the Song:");
-                    String title = scanner.next();
+                    String title = scanner.nextLine();
                     System.out.println("Please enter the artist of the Song:");
-                    String artist = scanner.next();
+                    String artist = scanner.nextLine();
                     System.out.println("Please enter the album of the Song:");
-                    String album = scanner.next();
+                    String album = scanner.nextLine();
                     System.out.println("Please enter the duration of the Song (in seconds):");
                     int duration = scanner.nextInt();
 
@@ -53,9 +54,9 @@ public class Menu {
 
                 case 5:
                     System.out.println("Please enter the name of the Playlist:");
-                    String namePlaylist5 = scanner.next();
+                    String namePlaylist5 = scanner.nextLine();
                     System.out.println("Please enter the title of the Song to delete:");
-                    String titleToDelete = scanner.next();
+                    String titleToDelete = scanner.nextLine();
                     Functions.deleteSong(namePlaylist5, titleToDelete);
                     break;
 
